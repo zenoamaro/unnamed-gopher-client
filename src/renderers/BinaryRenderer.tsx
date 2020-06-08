ddaf594e33f12216ee774a76a1e8782e16db200d
zenoamaro <zenoamaro@gmail.com>
2020-06-08 19:35:37 +0200

    Icons for binary files

--- src/renderers/BinaryRenderer.tsx
+++ src/renderers/BinaryRenderer.tsx
@@ -5,19 +5,36 @@ import * as Gopher from 'gopher';
 import {Vertical} from 'components/Layout';
 import LinkButton from 'components/LinkButton';
 import {RendererProps} from './Renderer';
+import Bag from 'utils/Bag';
+import * as Icons from 'react-icons/io';
 
+
+const mapping: Bag<React.FC<{size: number}>> = {
+  '.txt .md .html .pdf .doc .xls': Icons.IoIosDocument,
+  '.zip .rar .7z .tar .gz .sit .hqx': Icons.IoIosArchive,
+  '.mp3 .ogg .wav .mid': Icons.IoIosVolumeHigh,
+  '*': Icons.IoIosCube,
+};
 
 export default function BinaryRenderer(p: RendererProps) {
   return React.useMemo(() => {
     const {pathname} = Gopher.parseGopherUrl(p.url);
     const basename = Path.basename(pathname);
+    const extname = Path.extname(basename);
+
+    const Icon = Object.entries(mapping).reduce((Match, [exts, Component]) => {
+      return exts.split(' ').includes(extname) ? Component : Match;
+    }, mapping['*']);
 
     return <Container>
+      <IconContainer><Icon size={128}/></IconContainer>
       <Label>{basename}</Label>
       <LinkButton href={p.url} download={basename}>Save to disk</LinkButton>
     </Container>;
   }, [p.url, p.timestamp]);
 }
+
+
 
 const Container = styled(Vertical)`
   height: 100%;
@@ -28,6 +45,12 @@ const Container = styled(Vertical)`
   overflow: auto scroll;
 `;
 
+const IconContainer = styled.div`
+  display: contents;
+  color: #aaa;
+`;
+
 const Label = styled.div`
-  margin-bottom: 12px;
+  margin: 12px;
+  font-weight: bold;
 `;
